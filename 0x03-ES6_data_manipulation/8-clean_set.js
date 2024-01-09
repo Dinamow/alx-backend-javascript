14a045d738e1559130cddbed9740cd5b502163c6
DINAMOW <meemoo102039@gmail.com>
2024-01-09 22:03:29 +0200

    fix 8

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,4 +1,5 @@
 export default function cleanSet(set, string) {
+	if (!set || !string || string.length === 0) return '';
   let exmaple = [...set].filter((item) => item.includes(string));
   const len = string.length;
   for (const item of exmaple) {
