054b7cdd32ba852e13b1a961ecbd1ec1a0cae27d
DINAMOW <meemoo102039@gmail.com>
2024-01-09 21:24:43 +0200

    better

--- 0x03-ES6_data_manipulation/7-has_array_values.js
+++ 0x03-ES6_data_manipulation/7-has_array_values.js
@@ -1,8 +1,3 @@
 export default function hasValuesFromArray(array, values) {
-  for (const value of values) {
-    if (!array.includes(value)) {
-      return false;
-    }
-  }
-  return true;
+  return values.every((value) => array.includes(value));
 }
