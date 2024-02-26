006a3661b2fb0aeb9362a33bf602722ed1683422
DINAMOW <meemoo102039@gmail.com>
2024-02-26 18:23:03 +0200

    fix semistand

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,5 +1,4 @@
 const fs = require('fs');
-const { constrainedMemory } = require('process');
 
 const countStudents = (path) => {
   if (!fs.existsSync(path)) {
@@ -8,25 +7,25 @@ const countStudents = (path) => {
   if (!fs.statSync(path).isFile()) {
     throw new Error('Cannot load the database');
   }
-	const fileLines = fs.readFileSync(path
-		, 'utf-8')
-		.split('\n')
-		.filter((line) => line.length > 0);
-	console.log(`Number of students: ${fileLines.length - 1}`);
+  const fileLines = fs.readFileSync(path
+    , 'utf-8')
+    .split('\n')
+    .filter((line) => line.length > 0);
+  console.log(`Number of students: ${fileLines.length - 1}`);
   const studentGroups = {};
-	fileLines.slice(1).forEach((line) => {
-		const student = line.split(',');
-		if (!studentGroups[student[3]]) {
-			studentGroups[student[3]] = [];
-		}
-		studentGroups[student[3]].push(student[0]);
-	});
-	delete studentGroups.field;
-	for (const group in studentGroups) {
-		if (studentGroups[group].length > 0) {
-			console.log(`Number of students in ${group}: ${studentGroups[group].length}. List: ${studentGroups[group].join(', ')}`);
-		}
-	}
+  fileLines.slice(1).forEach((line) => {
+    const student = line.split(',');
+    if (!studentGroups[student[3]]) {
+      studentGroups[student[3]] = [];
+    }
+    studentGroups[student[3]].push(student[0]);
+  });
+  delete studentGroups.field;
+  for (const group in studentGroups) {
+    if (studentGroups[group].length > 0) {
+      console.log(`Number of students in ${group}: ${studentGroups[group].length}. List: ${studentGroups[group].join(', ')}`);
+    }
+  }
 };
 
 module.exports = countStudents;
