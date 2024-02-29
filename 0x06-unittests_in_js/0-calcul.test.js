39560064149a4636ce318b3bbf81943c2bedec2b
DINAMOW <meemoo102039@gmail.com>
2024-02-29 10:58:41 +0200

    fix the test cases

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,20 +1,24 @@
-const { describe } = require('mocha');
+const { describe, it } = require('mocha');
 const calculateNumber = require('./0-calcul');
 const assert = require('assert');
 
-describe('#calculateNumber', () => {
-	describe('add', () => {
-		it('should return 4', () => {
-			assert.strictEqual(calculateNumber(1, 3), 4);
-		});
-		it('should return 5', () => {
-			assert.strictEqual(calculateNumber(1.5, 3.5), 5);
-		});
-		it('should return 5', () => {
-			assert.strictEqual(calculateNumber(1.4, 3.5), 5);
-		});
-		it('should return 6', () => {
-			assert.strictEqual(calculateNumber(1.6, 3.5), 6);
-		});
+describe('calculateNumber', () => {
+	it('should return 4', () => {
+		assert.strictEqual(calculateNumber(1, 3), 4);
+	});
+	it('should return 5', () => {
+		assert.strictEqual(calculateNumber(1, 3.7), 5);
+	});
+	it('should return 5', () => {
+		assert.strictEqual(calculateNumber(1.2, 3.7), 5);
+	});
+	it('should return 6', () => {
+		assert.strictEqual(calculateNumber(1.5, 3.5), 6);
+	});
+	it('should return 5', () => {
+		assert.strictEqual(calculateNumber(1.4, 3.5), 5);
+	});
+	it('should return 6', () => {
+		assert.strictEqual(calculateNumber(1.6, 3.5), 6);
 	});
 });
